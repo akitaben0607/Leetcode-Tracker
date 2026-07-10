7756cbcd1098d86a02639dda14fa6d02d96aede4
akitaben0607 <akitaben176@gmail.com>
2026-07-10 11:53:18 +0530

    Create 3830-FindClosestPerson

---
+++ 3830-FindClosestPerson/3830-FindClosestPerson.java
@@ -0,0 +1,9 @@
+// Last updated: 7/10/2026, 11:53:17 AM
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int p1=Math.abs(x-z);
+        int p2=Math.abs(y-z);
+        if(p1==p2) return 0;
+        else return (p1<=p2)?1:2;
+    }
+}
